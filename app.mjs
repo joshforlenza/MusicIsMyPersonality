51b017e8cd4c99af717e7dd0d99796b06fc078a7
Joshua Forlenza <97926343+joshforlenza@users.noreply.github.com>
2022-11-21 21:50:51 -0500

    Adding more input to profile page

--- app.mjs
+++ app.mjs
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 import { URLSearchParams } from 'url';
 import fetch from 'node-fetch';
 import cookieParser from 'cookie-parser'
+import { Handlebars } from 'hbs';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
