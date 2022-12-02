147beb11473ec6af971e173bd46b3e5a8e958c60
Joshua Forlenza <97926343+joshforlenza@users.noreply.github.com>
2022-12-01 22:16:44 -0500

    Implementing playlist creation

--- app.mjs
+++ app.mjs
@@ -183,7 +183,7 @@ app.post('/summary', async (req, res) => {
         User.findOne({username: currUser.username}).exec(async (err, user) => {
             if(user && !err){
                 const response = await functions.useAccessToken("https://api.spotify.com/v1/me/top/tracks",req.session.user.authToken);
-                console.log(response);
+                //console.log(response);
                 if(response==="error"){ //get new token if current one expired
                     res.redirect('/refresh_token');
                 }
@@ -195,10 +195,10 @@ app.post('/summary', async (req, res) => {
                 const trackURIs = topTracks.reduce(function(pV, cV, cI){
                     pV.push(cV.uri);
                     return pV;
-                }, [])
-                console.log(trackURIs);
+                }, []);
+                console.log("URIs: "+trackURIs);
                 const res2 = await functions.createPlaylist(req.session.user.username, req.session.user.authToken);
-                console.log(res2);
+                //console.log(res2);
                 await functions.addToPlaylist(res2.id, trackURIs, req.session.user.authToken);
                 res.redirect('/summary');
             }
